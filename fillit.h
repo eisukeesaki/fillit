22c70777af7f41d3197fb5a3f4e8de33954d4f57
Eisuke Esaki <eesaki@student.42.us.org>
2019-05-17 22:12:52 -0700

    created: fillit.h

---
+++ fillit.h
@@ -0,0 +1,12 @@
+#ifndef FILLIT_H
+# define FILLIT_H
+
+# define BUFF_SIZE 546
+# define INPUT_MAX 5466
+
+# include "libft/libft.h"
+# include <fcntl.h>
+
+t_mino		*str_to_mino(char *input);
+
+#endif
