e343f3035bb460614902c52571ca9128e07f63ac
Eisuke Esaki <eesaki@student.42.us.org>
2019-05-17 22:21:59 -0700

    create array of structs of validated minos

--- read_tetromino.c
+++ read_tetromino.c
@@ -1,56 +1,58 @@
 #include "tetromino.h"
-#include "libft/libft.h"
-#define BUFF_SIZE 546
+#include "fillit.h"
 
-t_mino	*str_to_mino(char *input);
+size_t	g_mino_ct = 0;
 
-void	rd_input(const int fd, char *array)
+void	rd_input(const int fd, char *buff)
 {
 	int		rc;
 
 	if (fd < 0 || BUFF_SIZE <= 0)
 		return ;
+
 	while (1)
 	{
-		if ((rc = read(fd, array, BUFF_SIZE)) == 0)
+		if ((rc = read(fd, buff, BUFF_SIZE)) == 0)
 			break;
 	}
 	if (rc == -1)
 		return ;
 }
 
-size_t	count_mino(char *s)
+char	**buff_to_raw_minos(int const fd)
 {
-	return (ft_strlen(s) % 20);
-}
-
-char	**init_array_of_minos(size_t n)
-{
-	char	**array;
-
-	array = ft_memalloc((n + 1) * sizeof(char *));
-	return (array);
-}
-
-char	**split_input(int const fd)
-{
-	char	raw_input[BUFF_SIZE];
-	size_t	mino_ct;
-	char	**minos;
-	size_t	i;
+	char	input[INPUT_MAX];
+	char	**raw_minos;
 	size_t	head;
+	size_t	i;
+
+	rd_input(fd, input);
+	g_mino_ct = ft_strlen(input) / 21;
+	head = 0;
+	i = 0;
+	raw_minos = ft_memalloc(sizeof(char *) * (g_mino_ct + 1));
+	while (i < g_mino_ct)
+	{
+		raw_minos[i]= ft_strsub(input, head, 21);
+		head = head + 21;
+		i++;
+	}
+	return (raw_minos);
+}
+
+t_mino	**raw_minos_to_minos(size_t g_mino_ct, char **raw_minos)
+{
+	size_t	i;
+	t_mino	*mino;
+	t_mino	**minos;
 
 	i = 0;
-	head = 0;
-	rd_input(fd, raw_input);
-	mino_ct = ft_strlen(raw_input) % 20;
-	minos = init_array_of_minos(mino_ct);
-	while (i < mino_ct)
+	mino = NULL;
+	minos = ft_memalloc(sizeof(t_mino *) * g_mino_ct);
+	while (raw_minos[i])
 	{
-		minos[i] = ft_strsub(raw_input, head, 21);
+		minos[i] = str_to_mino(raw_minos[i]);
 		i++;
-		head = head + 21;
 	}
-	minos[i] = NULL;
 	return (minos);
 }
