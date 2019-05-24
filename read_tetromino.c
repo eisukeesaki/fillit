67fa9f273f860fcee8f318e9456a6dfee0d2294f
Eisuke Esaki <eesaki@student.42.us.org>
2019-05-23 20:57:29 -0700

    implemented fd_to_minos(), made functions static

--- read_tetromino.c
+++ read_tetromino.c
@@ -1,15 +1,12 @@
 #include "tetromino.h"
 #include "fillit.h"
 
-size_t	g_mino_ct = 0;
-
-void	rd_input(const int fd, char *buff)
+static void		read_input(const int fd, char *buff)
 {
 	int		rc;
 
 	if (fd < 0 || BUFF_SIZE <= 0)
 		return ;
-
 	while (1)
 	{
 		if ((rc = read(fd, buff, BUFF_SIZE)) == 0)
@@ -19,19 +16,16 @@ void	rd_input(const int fd, char *buff)
 		return ;
 }
 
-char	**buff_to_raw_minos(int const fd)
+static char		**buff_to_raw_minos(size_t mino_ct, char *input)
 {
-	char	input[INPUT_MAX];
 	char	**raw_minos;
 	size_t	head;
 	size_t	i;
 
-	rd_input(fd, input);
-	g_mino_ct = ft_strlen(input) / 21;
 	head = 0;
 	i = 0;
-	raw_minos = ft_memalloc(sizeof(char *) * (g_mino_ct + 1));
-	while (i < g_mino_ct)
+	raw_minos = ft_memalloc(sizeof(char *) * (mino_ct + 1));
+	while (i < mino_ct)
 	{
 		raw_minos[i]= ft_strsub(input, head, 21);
 		head = head + 21;
@@ -40,24 +34,36 @@ char	**buff_to_raw_minos(int const fd)
 	return (raw_minos);
 }
 
-t_mino	**raw_minos_to_minos(size_t g_mino_ct, char **raw_minos)
+static t_mino	**raw_minos_to_minos(size_t mino_ct, char **raw_minos)
 {
 	size_t	i;
 	t_mino	*mino;
 	t_mino	**minos;
-	// char	letter;
+	char	letter;
 
 	i = 0;
 	mino = NULL;
-	minos = ft_memalloc(sizeof(t_mino *) * g_mino_ct + 1);
-	// letter = 'A';
+	minos = ft_memalloc(sizeof(t_mino *) * mino_ct + 1);
+	letter = 'A';
 	while (raw_minos[i])
 	{
 		minos[i] = str_to_mino(raw_minos[i]);
-		// minos[i]->letter = letter;
-		// letter++;
+		minos[i]->letter = letter;
+		letter++;
 		i++;
 	}
 	return (minos);
+}
+
+t_mino			**fd_to_minos(int const fd)
+{
+	char	input[INPUT_MAX];
+	size_t	mino_ct;
+	char	**split_minos;
+
+	read_input(fd, input);
+	mino_ct = ft_strlen(input) / 21;
+	split_minos = buff_to_raw_minos(mino_ct, input);
+	return (raw_minos_to_minos(mino_ct, split_minos));
 }
 
