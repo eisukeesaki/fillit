5a44e888f65f155b0670b66770d86b3e5dfc6aea
Eisuke Esaki <eesaki@student.42.us.org>
2019-05-23 20:59:42 -0700

    added fd_to_minos() prototype

--- fillit.h
+++ fillit.h
@@ -5,8 +5,14 @@
 # define INPUT_MAX 546
 
 # include "libft/libft.h"
+# include "tetromino.h"
 # include <fcntl.h>
 
-t_mino		*str_to_mino(char *input);
+t_mino	**fd_to_minos(int const fd);
+t_mino	*str_to_mino(char *input);
+void	fillit(t_mino **minos);
+int		check(char board[][16], t_mino *mino, int x, int y);
+void	place(char board[][16], t_mino *mino, int x, int y);
+void	clear(char board[][16], t_mino *mino, int x, int y);
 
 #endif
